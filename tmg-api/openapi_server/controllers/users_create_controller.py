3248d48df7e558d79131f199eb5e5f76a9268311
Steven Hopkins <steve@themoderngroom.com>
2023-11-19 20:22:03 -0700

    Adding custom code for checkpoint, still getting positional argument user error

--- tmg-api/openapi_server/controllers/users_create_controller.py
+++ tmg-api/openapi_server/controllers/users_create_controller.py
@@ -6,22 +6,24 @@ from typing import Union
 from openapi_server.models.user import User  # noqa: E501
 from openapi_server import util
 
-from openapi_server.database.models import User  # Import the User model from your database models
+from openapi_server.database.database_manager import get_database_session
 
 
-def create_user(user):  # noqa: E501
+def create_user(username):  # noqa: E501
     """Create user
 
      # noqa: E501
 
-    :param user: 
+    :param username: 
     :type user: dict | bytes
 
     :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
     """
     if connexion.request.is_json:
         user_data = connexion.request.get_json()
-        new_user = User(name=user_data.get('name'), email=user_data.get('email'))
+        print(user_data)
+        session = get_database_session()
+        new_user = User(name=user_data.get('username'), email=user_data.get('email'))
         # Add the new user to the database session and commit
         session.add(new_user)
         session.commit()

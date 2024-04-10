d06cba60f316eead9c11d13ec90c86842078a03e
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-04-10 13:38:17 +0300

    update

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# make sure you have server dir in PYTHONPATH
-# export PYTHONPATH=${PYTHONPATH}:$(pwd)/server
+export PYTHONPATH=${PYTHONPATH}:$(pwd)/server
 
 python scripts/reset_db.py

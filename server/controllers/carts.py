8501829a21295deb0d8d7e0b168fff9576135bba
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-04-16 16:53:14 +0300

    Tests for carts/ endpoint (#43)

--- server/controllers/carts.py
+++ server/controllers/carts.py
@@ -1,8 +1,10 @@
-import uuid
+from flask import current_app as app, jsonify
 
 from server.controllers.hmac_1 import hmac_verification
 from server.controllers.shopify import *
-from server.database.models import Cart, CartProduct
+from server.database.database_manager import session_factory
+from server.services import ServiceError, NotFoundError
+from server.services.cart import CartService
 
 shopify_store = os.getenv("shopify_store")
 admin_api_access_token = os.getenv("admin_api_access_token")
@@ -12,211 +14,55 @@ db = get_database_session()
 
 @hmac_verification()
 def create_cart(cart):
-    """Create cart
+    cart_service = CartService(session_factory())
 
-    :param cart:
-    :type cart: dict | bytes
-    :rtype: None
-    """
     try:
-        cart_id = uuid.uuid4()
-        if "user_id" not in cart:
-            cart["user_id"] = None
-        new_cart = Cart(id=cart_id, user_id=cart["user_id"], event_id=cart["event_id"], attendee_id=cart["attendee_id"])
-        db.add(new_cart)
+        cart = cart_service.create_cart(**cart)
+    except ServiceError as e:
+        app.logger.error(e.message, e)
+        return jsonify({"errors": e.message}), 500
 
-        if "products" in cart:
-            for product in cart["products"]:
-                cart_prod_id = uuid.uuid4()
-                new_cart_product = CartProduct(
-                    id=cart_prod_id,
-                    cart_id=cart_id,
-                    product_id=product["product_id"],
-                    variation_id=product["variation_id"],
-                    category=product["category"],
-                    quantity=product["quantity"],
-                )
-                db.add(new_cart_product)
-
-        db.commit()
-        # return 'Cart created successfully!', 201
-        return cart_id
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return f"Internal Server Error : {e}", 500
+    return str(cart.id), 201
 
 
 @hmac_verification()
 def get_cart_by_id(cart_id):
-    """Retrieve a specific cart by ID
+    cart_service = CartService(session_factory())
 
-     # noqa: E501
-
-    :param cart_id: Unique identifier of the cart to retrieve
-    :type cart_id: int
-
-    :rtype: cart
-    """
     try:
-        cart = db.query(Cart).filter(Cart.id == cart_id).first()
-        if not cart:
-            return "Cart not found", 204
-        cart_products = db.query(CartProduct).filter(CartProduct.cart_id == cart_id).all()
-        response = {
-            "user_id": str(cart.user_id),
-            "event_id": str(cart.event_id),
-            "attendee_id": str(cart.attendee_id),
-            "products": [],
-        }
+        cart = cart_service.get_cart_by_id(cart_id)
+    except NotFoundError as e:
+        app.logger.debug(e.message, e)
+        return jsonify({"errors": e.message}), 404
 
-        for product in cart_products:
-
-            response["products"].append(
-                {
-                    "product_id": str(product.product_id),
-                    "variation_id": str(product.variation_id),
-                    "category": product.category,
-                    "quantity": product.quantity,
-                }
-            )
-
-        return response
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return f"Internal Server Error : {e}", 500
+    return cart, 200
 
 
 @hmac_verification()
-def get_cart_by_event_attendee(event_id, attendee_id):  # noqa: E501
-    """Retrieve a specific cart by ID
+def get_cart_by_event_attendee(event_id, attendee_id):
+    cart_service = CartService(session_factory())
 
-     # noqa: E501
-
-    :param cart_id: Unique identifier of the cart to retrieve
-    :type cart_id: int
-
-    :rtype: cart
-    """
     try:
-        cart = db.query(Cart).filter(Cart.event_id == event_id, Cart.attendee_id == attendee_id).first()
-        if not cart:
-            return "Cart not found", 204
-        cart_products = db.query(CartProduct).filter(CartProduct.cart_id == cart.id).all()
-        response = {
-            "user_id": str(cart.user_id),
-            "event_id": str(cart.event_id),
-            "attendee_id": str(cart.attendee_id),
-            "products": [],
-        }
-        for product in cart_products:
-            url = f"https://{shopify_store}.myshopify.com/admin/api/2024-01/products/{product.product_id}.json"
-            headers = {
-                "Content-Type": "application/json",
-                "X-Shopify-Access-Token": admin_api_access_token,
-            }
-            prod_response = requests.get(url, headers=headers)
-            data = None
-            if prod_response.status_code == 200:
-                prod_response = prod_response.json()
-                data = data_extractor(prod_response, product.variation_id)
-            else:
-                data = "Not Found"
-            response["products"].append(
-                {
-                    "product_id": str(product.product_id),
-                    "variation_id": str(product.variation_id),
-                    "category": product.category,
-                    "quantity": product.quantity,
-                    "Product_details": [data],
-                }
-            )
-
-        return response
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return f"Internal Server Error : {e}", 500
+        return cart_service.get_cart_by_event_attendee(event_id, attendee_id), 200
+    except NotFoundError as e:
+        app.logger.debug(e.message, e)
+        return jsonify({"errors": e.message}), 404
+    except ServiceError as e:
+        app.logger.error(e.message, e)
+        return jsonify({"errors": e.message}), 500
 
 
 @hmac_verification()
-def update_cart(cart):  # noqa: E501
-    """Update an existing cart by ID
-
-     # noqa: E501
-
-    :param cart_id: Unique identifier of the cart to update
-    :type cart_id: int
-    :param cart:
-    :type cart: dict | bytes
-    :rtype: None
-    """
-    try:
-        cart_id = cart["id"]
-        user_id = cart.get("user_id", None)
-        event_id = cart.get("event_id")
-        attendee_id = cart.get("attendee_id")
-
-        existing_cart = db.query(Cart).filter(Cart.id == cart_id).first()
-
-        if existing_cart:
-            existing_cart.user_id = user_id
-            existing_cart.event_id = event_id
-            existing_cart.attendee_id = attendee_id
-
-            db.commit()
-
-            if "products" in cart:
-                existing_cart_products = db.query(CartProduct).filter(CartProduct.cart_id == cart_id).all()
-
-                for cart_product in existing_cart_products:
-                    db.delete(cart_product)
-
-                for product in cart["products"]:
-                    cart_prod_id = uuid.uuid4()
-                    new_cart_product = CartProduct(
-                        id=cart_prod_id,
-                        cart_id=cart_id,
-                        product_id=product["product_id"],
-                        variation_id=product["variation_id"],
-                        category=product["category"],
-                        quantity=product["quantity"],
-                    )
-                    db.add(new_cart_product)
-
-                db.commit()
-            return "Cart updated successfully", 200
-        else:
-            return "Cart not found", 404
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return f"Internal Server Error : {e}", 500
-
-
-def data_extractor(response, variant_id):
+def update_cart(cart):
+    cart_service = CartService(session_factory())
 
     try:
-        product_details = response
-        product = product_details.get("product", {})
-        product_id = product.get("id")
-        title = product.get("title")
-        vendor = product.get("vendor")
-        body_html = product.get("body_html")
-        images = product.get("images")
-        variants = product.get("variants", [])
-        specific_variant = [variant for variant in variants if str(variant["id"]) == str(variant_id)]
-        if specific_variant:
-            specific_variant = specific_variant[0]
-        else:
-            specific_variant = None
+        cart = cart_service.update_cart(**cart)
+    except NotFoundError as e:
+        app.logger.debug(e.message, e)
+        return jsonify({"errors": e.message}), 404
+    except ServiceError as e:
+        app.logger.error(e.message, e)
+        return jsonify({"errors": e.message}), 500
 
-        return {
-            "product_id": product_id,
-            "title": title,
-            "vendor": vendor,
-            "body_html": body_html,
-            "images": images,
-            "specific_variant": specific_variant,
-        }
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return f"Internal Server Error : {e}", 500
+    return cart.to_dict(), 200
---
+++ server/test/test_carts.py
@@ -0,0 +1,288 @@
+from __future__ import absolute_import
+
+import json
+import uuid
+
+from server import encoder
+from server.database.models import Cart, CartProduct
+from server.services.attendee import AttendeeService
+from server.services.cart import CartService
+from server.services.event import EventService
+from server.services.look import LookService
+from server.services.role import RoleService
+from server.services.user import UserService
+from server.test import BaseTestCase, fixtures
+
+
+class TestCarts(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.look_service = LookService(self.session_factory)
+        self.role_service = RoleService(self.session_factory)
+        self.user_service = UserService(self.session_factory)
+        self.attendee_service = AttendeeService(self.session_factory)
+        self.event_service = EventService(self.session_factory)
+        self.cart_service = CartService(self.session_factory)
+
+    def assert_equal_cart(self, cart: Cart, request_cart: dict):
+        self.assertEqual(request_cart.get("user_id"), str(cart.user_id) if cart.user_id else None)
+        self.assertEqual(request_cart.get("event_id"), str(cart.event_id))
+        self.assertEqual(request_cart.get("attendee_id"), str(cart.attendee_id))
+
+    def assert_equal_cart_product(self, cart_product: CartProduct, request_cart_product: dict):
+        self.assertEqual(request_cart_product["product_id"], cart_product.product_id)
+        self.assertEqual(request_cart_product["variation_id"], cart_product.variation_id)
+        self.assertEqual(request_cart_product["category"], cart_product.category)
+        self.assertEqual(request_cart_product["quantity"], cart_product.quantity)
+
+    def test_create_cart_without_user_and_without_products(self):
+        # given
+        user = self.user_service.create_user(**fixtures.user_request())
+        event = self.event_service.create_event(**fixtures.event_request(email=user.email))
+        look = self.look_service.create_look(**fixtures.look_request(event_id=str(event.id), user_id=str(user.id)))
+        role = self.role_service.create_role(**fixtures.role_request(event_id=str(event.id), look_id=str(look.id)))
+        attendee = self.attendee_service.create_attendee(
+            **fixtures.attendee_request(email=user.email, event_id=str(event.id), role=str(role.id))
+        )
+
+        # when
+        cart_request = fixtures.create_cart_request(event_id=str(event.id), attendee_id=str(attendee.id))
+        del cart_request["user_id"]
+
+        response = self.client.open(
+            "/carts",
+            query_string=self.hmac_query_params,
+            data=json.dumps(cart_request, cls=encoder.CustomJSONEncoder),
+            method="POST",
+            headers=self.request_headers,
+            content_type=self.content_type,
+        )
+
+        # then
+        self.assertStatus(response, 201)
+        cart_id = response.json
+        self.assertIsNotNone(cart_id)
+        cart = self.db.query(Cart).filter_by(id=cart_id).one_or_none()
+        self.assert_equal_cart(cart, cart_request)
+        self.assertEqual(cart.cart_products.count(), 0)
+
+    def test_create_cart_with_user(self):
+        # given
+        user = self.user_service.create_user(**fixtures.user_request())
+        event = self.event_service.create_event(**fixtures.event_request(email=user.email))
+        look = self.look_service.create_look(**fixtures.look_request(event_id=str(event.id), user_id=str(user.id)))
+        role = self.role_service.create_role(**fixtures.role_request(event_id=str(event.id), look_id=str(look.id)))
+        attendee = self.attendee_service.create_attendee(
+            **fixtures.attendee_request(email=user.email, event_id=str(event.id), role=str(role.id))
+        )
+
+        # when
+        cart_request = fixtures.create_cart_request(
+            user_id=str(user.id), event_id=str(event.id), attendee_id=str(attendee.id)
+        )
+
+        response = self.client.open(
+            "/carts",
+            query_string=self.hmac_query_params,
+            data=json.dumps(cart_request, cls=encoder.CustomJSONEncoder),
+            method="POST",
+            headers=self.request_headers,
+            content_type=self.content_type,
+        )
+
+        # then
+        self.assertStatus(response, 201)
+        cart_id = response.json
+        self.assertIsNotNone(cart_id)
+        cart = self.db.query(Cart).filter_by(id=cart_id).one_or_none()
+        self.assertIsNotNone(cart)
+        self.assert_equal_cart(cart, cart_request)
+        self.assertEqual(cart.cart_products.count(), 0)
+
+    def test_create_cart_with_products(self):
+        # given
+        user = self.user_service.create_user(**fixtures.user_request())
+        event = self.event_service.create_event(**fixtures.event_request(email=user.email))
+        look = self.look_service.create_look(**fixtures.look_request(event_id=str(event.id), user_id=str(user.id)))
+        role = self.role_service.create_role(**fixtures.role_request(event_id=str(event.id), look_id=str(look.id)))
+        attendee = self.attendee_service.create_attendee(
+            **fixtures.attendee_request(email=user.email, event_id=str(event.id), role=str(role.id))
+        )
+
+        # when
+        cart_request = fixtures.create_cart_request(
+            user_id=str(user.id),
+            event_id=str(event.id),
+            attendee_id=str(attendee.id),
+            products=[fixtures.create_cart_product_request(), fixtures.create_cart_product_request()],
+        )
+
+        response = self.client.open(
+            "/carts",
+            query_string=self.hmac_query_params,
+            data=json.dumps(cart_request, cls=encoder.CustomJSONEncoder),
+            method="POST",
+            headers=self.request_headers,
+            content_type=self.content_type,
+        )
+
+        # then
+        self.assertStatus(response, 201)
+        cart_id = response.json
+        self.assertIsNotNone(cart_id)
+        cart = self.db.query(Cart).filter_by(id=cart_id).one_or_none()
+        self.assertIsNotNone(cart)
+        self.assert_equal_cart(cart, cart_request)
+        products = cart.cart_products.all()
+        self.assertEqual(len(products), 2)
+        product_1 = products[0]
+        self.assertIsNotNone(product_1)
+        self.assert_equal_cart_product(product_1, cart_request["products"][0])
+        product_2 = products[1]
+        self.assertIsNotNone(product_2)
+        self.assert_equal_cart_product(product_2, cart_request["products"][1])
+
+    def test_get_cart_by_id(self):
+        # given
+        user = self.user_service.create_user(**fixtures.user_request())
+        event = self.event_service.create_event(**fixtures.event_request(email=user.email))
+        look = self.look_service.create_look(**fixtures.look_request(event_id=str(event.id), user_id=str(user.id)))
+        role = self.role_service.create_role(**fixtures.role_request(event_id=str(event.id), look_id=str(look.id)))
+        attendee = self.attendee_service.create_attendee(
+            **fixtures.attendee_request(email=user.email, event_id=str(event.id), role=str(role.id))
+        )
+        cart = self.cart_service.create_cart(
+            user_id=str(user.id),
+            event_id=str(event.id),
+            attendee_id=str(attendee.id),
+            products=[
+                fixtures.create_cart_product_request(),
+                fixtures.create_cart_product_request(),
+                fixtures.create_cart_product_request(),
+            ],
+        )
+
+        # when
+        response = self.client.open(
+            "/cart_by_id",
+            query_string={**self.hmac_query_params.copy(), "cart_id": str(cart.id)},
+            method="GET",
+            headers=self.request_headers,
+            content_type=self.content_type,
+        )
+
+        # then
+        self.assertStatus(response, 200)
+        self.assert_equal_cart(cart, response.json)
+        products = response.json["products"]
+        self.assertEqual(len(products), 3)
+        cart_products = self.db.query(CartProduct).filter_by(cart_id=cart.id).all()
+        self.assert_equal_cart_product(cart_products[0], products[0])
+        self.assert_equal_cart_product(cart_products[1], products[1])
+        self.assert_equal_cart_product(cart_products[2], products[2])
+
+    def test_get_cart_by_id_not_found(self):
+        # when
+        response = self.client.open(
+            "/cart_by_id",
+            query_string={**self.hmac_query_params.copy(), "cart_id": str(uuid.uuid4())},
+            method="GET",
+            headers=self.request_headers,
+            content_type=self.content_type,
+        )
+
+        # then
+        self.assertStatus(response, 404)
+
+    def test_get_cart_by_event_attendee(self):
+        # given
+        user = self.user_service.create_user(**fixtures.user_request())
+        event = self.event_service.create_event(**fixtures.event_request(email=user.email))
+        look = self.look_service.create_look(**fixtures.look_request(event_id=str(event.id), user_id=str(user.id)))
+        role = self.role_service.create_role(**fixtures.role_request(event_id=str(event.id), look_id=str(look.id)))
+        attendee = self.attendee_service.create_attendee(
+            **fixtures.attendee_request(email=user.email, event_id=str(event.id), role=str(role.id))
+        )
+        cart = self.cart_service.create_cart(
+            user_id=str(user.id),
+            event_id=str(event.id),
+            attendee_id=str(attendee.id),
+            products=[
+                fixtures.create_cart_product_request(),
+            ],
+        )
+
+        # when
+        response = self.client.open(
+            "/cart_by_event_attendee",
+            query_string={**self.hmac_query_params, "event_id": str(event.id), "attendee_id": str(attendee.id)},
+            method="GET",
+            headers=self.request_headers,
+            content_type=self.content_type,
+        )
+
+        # then
+        self.assertStatus(response, 200)
+        self.assert_equal_cart(cart, response.json)
+        products = response.json["products"]
+        self.assertEqual(len(products), 1)
+
+    def test_update_cart_not_found(self):
+        # when
+        update_cart_request = fixtures.update_cart_request()
+
+        response = self.client.open(
+            "/carts",
+            query_string=self.hmac_query_params,
+            data=json.dumps(update_cart_request, cls=encoder.CustomJSONEncoder),
+            method="PUT",
+            headers=self.request_headers,
+            content_type=self.content_type,
+        )
+
+        # then
+        self.assertStatus(response, 404)
+
+    def test_update_cart(self):
+        # when
+        user = self.user_service.create_user(**fixtures.user_request())
+        event = self.event_service.create_event(**fixtures.event_request(email=user.email))
+        look = self.look_service.create_look(**fixtures.look_request(event_id=str(event.id), user_id=str(user.id)))
+        role = self.role_service.create_role(**fixtures.role_request(event_id=str(event.id), look_id=str(look.id)))
+        attendee = self.attendee_service.create_attendee(
+            **fixtures.attendee_request(email=user.email, event_id=str(event.id), role=str(role.id))
+        )
+        cart = self.cart_service.create_cart(
+            user_id=str(user.id),
+            event_id=str(event.id),
+            attendee_id=str(attendee.id),
+            products=[
+                fixtures.create_cart_product_request(),
+                fixtures.create_cart_product_request(),
+                fixtures.create_cart_product_request(),
+            ],
+        )
+
+        update_cart_request = fixtures.update_cart_request(
+            id=str(cart.id),
+            user_id=str(user.id),
+            event_id=str(event.id),
+            attendee_id=str(attendee.id),
+            products=[
+                fixtures.create_cart_product_request(),
+                fixtures.create_cart_product_request(),
+            ],
+        )
+
+        response = self.client.open(
+            "/carts",
+            query_string=self.hmac_query_params,
+            data=json.dumps(update_cart_request, cls=encoder.CustomJSONEncoder),
+            method="PUT",
+            headers=self.request_headers,
+            content_type=self.content_type,
+        )
+
+        # then
+        self.assertStatus(response, 200)

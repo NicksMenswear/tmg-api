aaa6414083b00cad0c8ab5ea8f9c38338ecc70fd
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-04-24 16:32:38 +0300

    update

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -11,14 +11,13 @@ EMAIL_ACCOUNT_USERNAME = "e2etmg@hotmail.com"
 EMAIL_ACCOUNT_PASSWORD = "fbb06fc8-fd64-11ee-8a70-d73cbe5bfd61"
 
 STORE_CONFIG = {
-    "dev": {"url": "https://quickstart-a91e1214.myshopify.com", "password": "test123"},
-    "stg": {"url": "https://tmg-staging.myshopify.com", "password": "test123"},
+    "dev": {"url": "https://quickstart-a91e1214.myshopify.com", "password": "test123", "require_store_password": True},
+    "stg": {"url": "https://tmg-staging.myshopify.com", "password": "test123", "require_store_password": True},
 }
 
-STORE_URL = STORE_CONFIG[ACTIVE_ENV]["url"]
-REQUIRE_STORE_PASSWORD = STORE_CONFIG[ACTIVE_ENV]("config_require_store_password", True)
-STORE_PASSWORD = STORE_CONFIG[ACTIVE_ENV]["password"]
-TEST_PASSWORD_RESET_EMAIL = STORE_CONFIG[ACTIVE_ENV]["password_reset_email"]
+STORE_URL = STORE_CONFIG.get(ACTIVE_ENV).get("url")
+REQUIRE_STORE_PASSWORD = STORE_CONFIG.get(ACTIVE_ENV).get("require_store_password")
+STORE_PASSWORD = STORE_CONFIG.get(ACTIVE_ENV).get("password")
 
 API_PARAMS = {
     "dev": {

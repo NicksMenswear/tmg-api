08d6b67e2843e6da8a7ecd03de6e9c7b9f304629
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-05-02 21:10:05 +0300

    Fixed events by email to get if user is owner (#69)

--- server/controllers/events.py
+++ server/controllers/events.py
@@ -29,6 +29,19 @@ def create_event(event):
 
 
 @hmac_verification
+def list_events_attendees(email):
+    event_service = EventService()
+
+    try:
+        events = event_service.get_events_with_attendees_by_user_email(email)
+    except NotFoundError as e:
+        logger.debug(e.message, e)
+        return jsonify({"errors": e.message}), 404
+
+    return events, 200
+
+
+@hmac_verification
 def get_events_for_user(email, include_attendees=False):
     event_service = EventService()
 

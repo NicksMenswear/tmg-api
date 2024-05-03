fe7d381ba1043aa0ae7cd1f97558e9ff51f04d35
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-05-03 13:37:38 +0300

    update

--- scripts/reset_db.py
+++ scripts/reset_db.py
@@ -1,3 +1,5 @@
+from sqlalchemy import text
+
 from server.app import init_app, init_db
 from server.database.database_manager import db
 
@@ -7,5 +9,18 @@ if __name__ == "__main__":
     app = init_app()
     init_db()
     with app.app.app_context():
+        conn = db.engine.connect()
+        trans = conn.begin()
+
+        try:
+            create_extension = text('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+            conn.execute(create_extension)
+            trans.commit()
+        except Exception as e:
+            trans.rollback()
+            raise e
+        finally:
+            conn.close()
+
         db.drop_all()
         db.create_all()

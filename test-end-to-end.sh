cd00c6514ca13ad699522880912cf13be02cc475
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-05-03 11:54:06 +0300

    update

--- test-end-to-end.sh
+++ test-end-to-end.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 export ACTIVE_ENV=${ACTIVE_ENV:-stg}
+export BROWSER=${BROWSER:-chromium}
 
-echo "Running e2e against '${ACTIVE_ENV}' environment."
+echo "Running e2e against '${ACTIVE_ENV}' environment in '${BROWSER}' browser."
 
 docker-compose up --build --abort-on-container-exit test-end-to-end

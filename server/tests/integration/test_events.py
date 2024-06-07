7eb534c296d500e6946524e92d887d18ba1043e5
Max Lobur <max_lobur@outlook.com>
2024-06-07 15:17:27 +0200

    updates

--- server/tests/integration/test_events.py
+++ server/tests/integration/test_events.py
@@ -129,7 +129,7 @@ class TestEvents(BaseTestCase):
         self.assertEqual(created_event.get("user_id"), str(event_request.user_id))
         self.assertEqual(created_event.get("type"), str(EventTypeModel.OTHER))
         roles = self.role_service.get_roles_for_event(created_event.get("id"))
-        self.assertEqual(roles, [])
+        self.assertEqual(roles, set(PREDEFINED_ROLES[EventTypeModel.OTHER]))
 
     def test_create_event_without_type(self):
         # given

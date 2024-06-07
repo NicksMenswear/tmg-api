1a204d7acf5e079ed113e60964a54b536cf823eb
Max Lobur <max_lobur@outlook.com>
2024-06-07 15:21:15 +0200

    updates

--- server/tests/integration/test_users.py
+++ server/tests/integration/test_users.py
@@ -191,7 +191,7 @@ class TestUsers(BaseTestCase):
         response_event1 = response.json[0]
         self.assertEqual(response_event1["id"], str(event1.id))
         self.assertEqual(response_event1["status"], str(EventUserStatus.OWNER))
-        self.assertEqual(len(response_event1["roles"]), 1)
+        self.assertEqual(len(response_event1["roles"]), 2)
         self.assertEqual(response_event1["roles"][0]["id"], str(role11.id))
         self.assertEqual(len(response_event1["looks"]), 1)
         self.assertEqual(response_event1["looks"][0]["id"], str(look.id))
@@ -202,7 +202,7 @@ class TestUsers(BaseTestCase):
         response_event2 = response.json[1]
         self.assertEqual(response_event2["id"], str(event2.id))
         self.assertEqual(response_event2["status"], str(EventUserStatus.OWNER))
-        self.assertEqual(len(response_event2["roles"]), 0)
+        self.assertEqual(len(response_event2["roles"]), 1)
         self.assertEqual(response_event1["looks"][0]["id"], str(look.id))
         self.assertEqual(len(response_event1["attendees"]), 1)
         self.assertEqual(len(response_event2["attendees"]), 0)

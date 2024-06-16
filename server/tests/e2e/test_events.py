6540364a8d46bda7326efb0e2a0dee96fbad2ea7
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-06-16 18:39:22 +0300

    update

--- server/tests/e2e/test_events.py
+++ server/tests/e2e/test_events.py
@@ -1,3 +1,6 @@
+import unittest
+import uuid
+
 from playwright.sync_api import Page
 
 from server.tests import utils
@@ -306,3 +309,51 @@ def test_roles_persistence(page: Page):
     actions.open_event_accordion(page, event_id)
 
     verify.role_is_selected_for_attendee(page, event_id, attendee_id, role_id)
+
+
+@unittest.skip("This test is incomplete due to bug in invites")
+def test_set_role_and_verify_it_showed_up_for_attendee(page: Page):
+    event_name = utils.generate_event_name()
+    attendee_first_name = utils.generate_unique_name()
+    attendee_last_name = utils.generate_unique_name()
+    attendee_email = f"e2etmg+{utils.generate_unique_string()}@hotmail.com"
+    attendee_password = str(uuid.uuid4())
+
+    api.delete_all_events(TEST_USER_EMAIL)
+    actions.access_store(page)
+    actions.login(page, TEST_USER_EMAIL, TEST_USER_PASSWORD)
+
+    verify.no_upcoming_events_visible(page)
+
+    event_id = actions.create_new_event(page, event_name, EVENT_DATE, "prom")
+    attendee_id = actions.add_first_attendee(page, attendee_first_name, attendee_last_name, attendee_email)
+    actions.open_event_accordion(page, event_id)
+
+    role_name = "Attendee Parent or Chaperone"
+    role_id = actions.select_role_for_attendee(page, event_id, attendee_id, role_name)
+
+    actions.logout(page)
+
+    # email_content = email.look_for_email("Registration email", EMAIL_FROM, attendee_email)
+    # assert email_content is not None
+    #
+    # activation_link = email.link_from_email(email_content)
+    #
+    # assert activation_link is not None
+    #
+    # page.goto(activation_link)
+    #
+    # page.locator("#customer_password").fill(attendee_password)
+    # page.locator("#customer_password_confirmation").fill(attendee_password)
+    # page.get_by_role("button", name="Activate Account").click()
+    #
+    # confirmation_email_body = email.look_for_email("Customer account confirmation", None, attendee_email, 300)
+    # assert "You've activated your customer account." in confirmation_email_body
+    #
+    # page.goto(f"{STORE_URL}/account")
+    #
+    # event_item = page.locator(f'.tmg-invite-event-name:has-text("{event_name}")')
+    # event_item.scroll_into_view_if_needed()
+    # event_item.wait_for(state="visible")
+    #
+    # expect(page.get_by_text(role_name)).to_be_visible()
--- server/tests/e2e/test_invites.py
+++ server/tests/e2e/test_invites.py
@@ -37,26 +37,8 @@ def test_invite_attendee(page: Page):
 
     actions.send_invites_to_attendees_by_id(page, event_id, [attendee_id])
 
-    # verify email sent
+    actions.logout(page)
 
-    print(look_id)
-
-    # select role and look
-    # open send invite modal
-
-    # create look via api
-
-    # open send invite modal
-    # verify `No look and/or role selected` visible
-
-    # close dialog
-    # select role
-
-    # attendee_id = ui.add_attendee(page, event_id, attendee_first_name, attendee_last_name, attendee_email)
-    # assert attendee_id is not None
-    #
-    # ui.logout(page)
-    #
     # email_content = email.look_for_email("The Modern Groom: Account Created", EMAIL_FROM, attendee_email)
     # assert email_content is not None
     #
@@ -78,51 +60,3 @@ def test_invite_attendee(page: Page):
     # event_item = page.locator(f'.tmg-invite-event-name:has-text("{event_name}")')
     # event_item.scroll_into_view_if_needed()
     # event_item.wait_for(state="visible")
-
-
-# def test_set_role_and_verify_it_showed_up_for_attendee(page: Page):
-#     event_name = utils.generate_event_name()
-#     attendee_first_name = utils.generate_unique_name()
-#     attendee_last_name = utils.generate_unique_name()
-#     attendee_email = f"e2etmg+{utils.generate_unique_string()}@hotmail.com"
-#     attendee_password = str(uuid.uuid4())
-#
-#     api.delete_all_events(TEST_USER_EMAIL)
-#     ui.access_store(page)
-#     ui.login(page, TEST_USER_EMAIL, TEST_USER_PASSWORD)
-#
-#     expect(page.get_by_text("No Upcoming Events.").first).to_be_visible()
-#
-#     event_id = ui.create_new_event(page, event_name, EVENT_DATE, "prom")
-#     ui.open_event_accordion(page, event_id)
-#     attendee_id = ui.add_attendee(page, event_id, attendee_first_name, attendee_last_name, attendee_email)
-#     assert attendee_id is not None
-#
-#     role_name = "Attendee Parent or Chaperone"
-#     role_id = ui.select_role_for_attendee(page, event_id, attendee_id, role_name)
-#
-#     ui.logout(page)
-#
-#     email_content = email.look_for_email("Registration email", EMAIL_FROM, attendee_email)
-#     assert email_content is not None
-#
-#     activation_link = email.link_from_email(email_content)
-#
-#     assert activation_link is not None
-#
-#     page.goto(activation_link)
-#
-#     page.locator("#customer_password").fill(attendee_password)
-#     page.locator("#customer_password_confirmation").fill(attendee_password)
-#     page.get_by_role("button", name="Activate Account").click()
-#
-#     confirmation_email_body = email.look_for_email("Customer account confirmation", None, attendee_email, 300)
-#     assert "You've activated your customer account." in confirmation_email_body
-#
-#     page.goto(f"{STORE_URL}/account")
-#
-#     event_item = page.locator(f'.tmg-invite-event-name:has-text("{event_name}")')
-#     event_item.scroll_into_view_if_needed()
-#     event_item.wait_for(state="visible")
-#
-#     expect(page.get_by_text(role_name)).to_be_visible()

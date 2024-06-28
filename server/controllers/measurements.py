d5f946b22ae4bb9b6fb100eda4639780de6357f9
Max Lobur <max_lobur@outlook.com>
2024-06-28 20:37:36 +0200

    updates

---
+++ server/controllers/measurements.py
@@ -0,0 +1,15 @@
+import logging
+
+from server.controllers.util import error_handler
+from server.flask_app import FlaskApp
+
+logger = logging.getLogger(__name__)
+
+
+@error_handler
+def create(user_id, data):
+    measurement_service = FlaskApp.current().measurement_service
+
+    measurement_id = measurement_service.create(data)
+
+    return {"id": measurement_id}, 201

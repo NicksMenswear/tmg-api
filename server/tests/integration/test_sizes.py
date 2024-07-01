bf864f008ff2547a39824f0b2de1fd3239b70ff9
Max Lobur <max_lobur@outlook.com>
2024-07-01 15:07:08 +0200

    updates

--- server/tests/integration/test_sizes.py
+++ server/tests/integration/test_sizes.py
@@ -7,8 +7,13 @@ class TestSizes(BaseTestCase):
     def test_create_sizes(self):
         # given
         user = self.user_service.create_user(fixtures.create_user_request())
-        attendee1 = self.attendee_service.create_attendee(fixtures.create_attendee_request(user_id=user.id))
-        attendee2 = self.attendee_service.create_attendee(fixtures.create_attendee_request(user_id=user.id))
+        event = self.event_service.create_event(fixtures.create_event_request())
+        attendee1 = self.attendee_service.create_attendee(
+            fixtures.create_attendee_request(user_id=user.email, event_id=event.id)
+        )
+        attendee2 = self.attendee_service.create_attendee(
+            fixtures.create_attendee_request(user_id=user.email, event_id=event.id)
+        )
 
         # when
         response = self.client.open(
@@ -23,14 +28,16 @@ class TestSizes(BaseTestCase):
         self.assertStatus(response, 201)
         self.assertIsNotNone(response.json["id"])
 
-        # Size was set for existing attendee
+        # Size was set for existing attendees
         attendee1 = self.attendee_service.get_attendee(attendee1.id)
         self.assertIsTrue(attendee1.size)
         attendee2 = self.attendee_service.get_attendee(attendee2.id)
         self.assertIsTrue(attendee2.size)
 
         # Size is set for every new attendee
-        attendee3 = self.attendee_service.create_attendee(fixtures.create_attendee_request(user_id=user.id))
+        attendee3 = self.attendee_service.create_attendee(
+            fixtures.create_attendee_request(user_id=user.email, event_id=event.id)
+        )
         self.assertIsTrue(attendee3.size)
         attendee3 = self.attendee_service.get_attendee(attendee3.id)
         self.assertIsTrue(attendee3.size)

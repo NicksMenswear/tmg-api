26843bb96ce9a41f3e868b6d85e967ce68437d3b
Max Lobur <max_lobur@outlook.com>
2024-07-01 14:31:43 +0200

    updates

--- server/tests/integration/test_sizes.py
+++ server/tests/integration/test_sizes.py
@@ -7,6 +7,8 @@ class TestSizes(BaseTestCase):
     def test_create_sizes(self):
         # given
         user = self.user_service.create_user(fixtures.create_user_request())
+        attendee1 = self.attendee_service.create_attendee(fixtures.create_attendee_request(user_id=user.id))
+        attendee2 = self.attendee_service.create_attendee(fixtures.create_attendee_request(user_id=user.id))
 
         # when
         response = self.client.open(
@@ -20,3 +22,15 @@ class TestSizes(BaseTestCase):
         # then
         self.assertStatus(response, 201)
         self.assertIsNotNone(response.json["id"])
+
+        # Size was set for existing attendee
+        attendee1 = self.attendee_service.get_attendee(attendee1.id)
+        self.assertIsTrue(attendee1.size)
+        attendee2 = self.attendee_service.get_attendee(attendee2.id)
+        self.assertIsTrue(attendee2.size)
+
+        # Size is set for every new attendee
+        attendee3 = self.attendee_service.create_attendee(fixtures.create_attendee_request(user_id=user.id))
+        self.assertIsTrue(attendee3.size)
+        attendee3 = self.attendee_service.get_attendee(attendee3.id)
+        self.assertIsTrue(attendee3.size)

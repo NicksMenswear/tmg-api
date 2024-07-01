e4f84e92e3ab3505dab02c3855d25351149b551e
Max Lobur <max_lobur@outlook.com>
2024-07-01 15:15:35 +0200

    updates

--- server/tests/integration/test_sizes.py
+++ server/tests/integration/test_sizes.py
@@ -4,7 +4,24 @@ from server.tests.integration import BaseTestCase, fixtures
 
 
 class TestSizes(BaseTestCase):
-    def test_create_sizes(self):
+    def test_create_size(self):
+        # given
+        user = self.user_service.create_user(fixtures.create_user_request())
+
+        # when
+        response = self.client.open(
+            "/sizes",
+            method="POST",
+            headers=self.request_headers,
+            content_type=self.content_type,
+            data=json.dumps(fixtures.store_size_request(user_id=str(user.id))),
+        )
+
+        # then
+        self.assertStatus(response, 201)
+        self.assertIsNotNone(response.json["id"])
+
+    def test_create_size_with_existing_attendees(self):
         # given
         user = self.user_service.create_user(fixtures.create_user_request())
         event = self.event_service.create_event(fixtures.create_event_request())
@@ -34,10 +51,18 @@ class TestSizes(BaseTestCase):
         attendee2 = self.attendee_service.get_attendee(attendee2.id)
         self.assertIsTrue(attendee2.size)
 
-        # Size is set for every new attendee
-        attendee3 = self.attendee_service.create_attendee(
+    def test_create_size_and_new_attendee(self):
+        # given
+        user = self.user_service.create_user(fixtures.create_user_request())
+        event = self.event_service.create_event(fixtures.create_event_request())
+        self.size_service.create_size(fixtures.store_size_request(user_id=str(user.id)))
+
+        # then
+        attendee = self.attendee_service.create_attendee(
             fixtures.create_attendee_request(user_id=user.email, event_id=event.id)
         )
-        self.assertIsTrue(attendee3.size)
-        attendee3 = self.attendee_service.get_attendee(attendee3.id)
-        self.assertIsTrue(attendee3.size)
+
+        # Size is set for every new attendee
+        self.assertIsTrue(attendee.size)
+        attendee = self.attendee_service.get_attendee(attendee.id)
+        self.assertIsTrue(attendee.size)

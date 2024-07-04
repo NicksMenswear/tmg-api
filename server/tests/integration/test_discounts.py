df4485b738775bc52d58407d4b853437c30023df
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-07-04 12:54:36 +0300

    update

--- server/tests/integration/test_discounts.py
+++ server/tests/integration/test_discounts.py
@@ -1106,7 +1106,154 @@ class TestDiscounts(BaseTestCase):
         self.assertStatus(response, 400)
         self.assertTrue("Groom gift discount already issued for attendee" in response.json["errors"])
 
-    def test_get_owner_discounts_from_event_with_4_attendees(self):
+    def test_no_tmg_group_discount_for_event_with_less_then_4_attendees(self):
+        # given
+        user = self.app.user_service.create_user(fixtures.create_user_request())
+        event = self.app.event_service.create_event(fixtures.create_event_request(user_id=user.id))
+        attendee_user1 = self.app.user_service.create_user(fixtures.create_user_request())
+        attendee_user2 = self.app.user_service.create_user(fixtures.create_user_request())
+        attendee_user3 = self.app.user_service.create_user(fixtures.create_user_request())
+        look1 = self.app.look_service.create_look(
+            fixtures.create_look_request(user_id=attendee_user1.id, product_specs={"bundle": {"variant_id": 26}})
+        )
+        look2 = self.app.look_service.create_look(
+            fixtures.create_look_request(user_id=attendee_user2.id, product_specs={"bundle": {"variant_id": 31}})
+        )
+        attendee1 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user1.email, event_id=event.id, look_id=look1.id, invite=True, style=True
+            )
+        )
+        attendee2 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user2.email, event_id=event.id, look_id=look2.id, invite=True, style=True
+            )
+        )
+        attendee3 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user3.email, event_id=event.id, look_id=look2.id, invite=True, style=True
+            )
+        )
+
+        # when
+        response = self.client.open(
+            f"/events/{event.id}/discounts",
+            query_string=self.hmac_query_params,
+            method="GET",
+            content_type=self.content_type,
+            headers=self.request_headers,
+        )
+
+        # then
+        self.assert200(response)
+        self.assertEqual(len(response.json), 3)
+
+        for discount in response.json:
+            self.assertTrue(len(discount["gift_codes"]) == 0)
+
+    def test_no_tmg_group_discount_for_event_with_4_attendees_but_one_is_not_styled(self):
+        # given
+        user = self.app.user_service.create_user(fixtures.create_user_request())
+        event = self.app.event_service.create_event(fixtures.create_event_request(user_id=user.id))
+        attendee_user1 = self.app.user_service.create_user(fixtures.create_user_request())
+        attendee_user2 = self.app.user_service.create_user(fixtures.create_user_request())
+        attendee_user3 = self.app.user_service.create_user(fixtures.create_user_request())
+        attendee_user4 = self.app.user_service.create_user(fixtures.create_user_request())
+        look1 = self.app.look_service.create_look(
+            fixtures.create_look_request(user_id=attendee_user1.id, product_specs={"bundle": {"variant_id": 26}})
+        )
+        look2 = self.app.look_service.create_look(
+            fixtures.create_look_request(user_id=attendee_user2.id, product_specs={"bundle": {"variant_id": 31}})
+        )
+        attendee1 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user1.email, event_id=event.id, look_id=look1.id, invite=True, style=True
+            )
+        )
+        attendee2 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user2.email, event_id=event.id, look_id=look2.id, invite=True, style=True
+            )
+        )
+        attendee3 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user3.email, event_id=event.id, look_id=look2.id, invite=True, style=True
+            )
+        )
+        attendee4 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user4.email, event_id=event.id, look_id=None, invite=True, style=False
+            )
+        )
+
+        # when
+        response = self.client.open(
+            f"/events/{event.id}/discounts",
+            query_string=self.hmac_query_params,
+            method="GET",
+            content_type=self.content_type,
+            headers=self.request_headers,
+        )
+
+        # then
+        self.assert200(response)
+        self.assertEqual(len(response.json), 4)
+
+        for discount in response.json:
+            self.assertTrue(len(discount["gift_codes"]) == 0)
+
+    def test_no_tmg_group_discount_for_event_with_4_attendees_but_one_is_not_invited(self):
+        # given
+        user = self.app.user_service.create_user(fixtures.create_user_request())
+        event = self.app.event_service.create_event(fixtures.create_event_request(user_id=user.id))
+        attendee_user1 = self.app.user_service.create_user(fixtures.create_user_request())
+        attendee_user2 = self.app.user_service.create_user(fixtures.create_user_request())
+        attendee_user3 = self.app.user_service.create_user(fixtures.create_user_request())
+        attendee_user4 = self.app.user_service.create_user(fixtures.create_user_request())
+        look1 = self.app.look_service.create_look(
+            fixtures.create_look_request(user_id=attendee_user1.id, product_specs={"bundle": {"variant_id": 26}})
+        )
+        look2 = self.app.look_service.create_look(
+            fixtures.create_look_request(user_id=attendee_user2.id, product_specs={"bundle": {"variant_id": 31}})
+        )
+        attendee1 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user1.email, event_id=event.id, look_id=look1.id, invite=True, style=True
+            )
+        )
+        attendee2 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user2.email, event_id=event.id, look_id=look2.id, invite=True, style=True
+            )
+        )
+        attendee3 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user3.email, event_id=event.id, look_id=look2.id, invite=True, style=True
+            )
+        )
+        attendee4 = self.app.attendee_service.create_attendee(
+            fixtures.create_attendee_request(
+                email=attendee_user4.email, event_id=event.id, look_id=None, invite=False, style=True
+            )
+        )
+
+        # when
+        response = self.client.open(
+            f"/events/{event.id}/discounts",
+            query_string=self.hmac_query_params,
+            method="GET",
+            content_type=self.content_type,
+            headers=self.request_headers,
+        )
+
+        # then
+        self.assert200(response)
+        self.assertEqual(len(response.json), 4)
+
+        for discount in response.json:
+            self.assertTrue(len(discount["gift_codes"]) == 0)
+
+    def test_tmg_group_discounts_from_event_with_4_attendees(self):
         # given
         user = self.app.user_service.create_user(fixtures.create_user_request())
         event = self.app.event_service.create_event(fixtures.create_event_request(user_id=user.id))
@@ -1159,8 +1306,12 @@ class TestDiscounts(BaseTestCase):
 
             if str(discount_attendee_id) == str(attendee1.id):
                 self.assertEqual(discount["gift_codes"][0]["code"], TMG_GROUP_50_USD_OFF_DISCOUNT_CODE_PREFIX)
+                self.assertEqual(discount["gift_codes"][0]["amount"], 50)
             else:
                 self.assertEqual(discount["gift_codes"][0]["code"], TMG_GROUP_25_PERCENT_OFF_DISCOUNT_CODE_PREFIX)
+                self.assertEqual(
+                    discount["gift_codes"][0]["amount"], look2.product_specs["bundle"]["variant_id"] * 10 * 0.25
+                )
 
     def test_apply_discounts_invalid_attendee(self):
         # when

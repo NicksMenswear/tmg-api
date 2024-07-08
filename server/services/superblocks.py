109ef3fbd7e5a53a3f7a08d9f50dbcda31207e45
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-07-08 19:02:44 +0300

    Generate order number (#172)
    * Generate order number
    
    * update

--- server/services/superblocks.py
+++ server/services/superblocks.py
@@ -1,27 +1,18 @@
 import json
 import logging
-import os
-import uuid
-from abc import ABC, abstractmethod
+from abc import ABC
 
 from server.controllers.util import http
-from server.services import ServiceError
 
 logger = logging.getLogger(__name__)
 
 
-SUPERBLOCKS_ORDER_WEBHOOK_API_URL = os.getenv("SUPERBLOCKS_ORDER_WEBHOOK_API_URL")
-
-
 class AbstractSuperblocksService(ABC):
-    @abstractmethod
-    def generate_order_number(self):
-        return str(uuid.uuid4())
+    pass
 
 
 class FakeSuperblocksService(AbstractSuperblocksService):
-    def generate_order_number(self):
-        return str(uuid.uuid4())
+    pass
 
 
 class SuperblocksService(AbstractSuperblocksService):
@@ -34,11 +25,3 @@ class SuperblocksService(AbstractSuperblocksService):
         )
 
         return response.status, json.loads(response.data.decode("utf-8"))
-
-    def generate_order_number(self):
-        status, body = self.api_request("POST", SUPERBLOCKS_ORDER_WEBHOOK_API_URL)
-
-        if status >= 400:
-            raise ServiceError(f"Failed to generate order number. {body}")
-
-        return body.get("output", {}).get("result")

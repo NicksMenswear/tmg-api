d56886e34b21db4e275c758d203811b1fcf95701
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-07-15 19:05:00 +0300

    update

--- alembic/versions/d8763990bec6_webhooks_table.py
+++ alembic/versions/d8763990bec6_webhooks_table.py
@@ -8,9 +8,9 @@ Create Date: 2024-07-15 19:02:34.386257
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
+
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "d8763990bec6"

02f859c80970655ae13b626392a91e0e5d2f9a94
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-07-16 19:09:43 +0300

    Set ship_by_date (#195)

--- server/models/order_model.py
+++ server/models/order_model.py
@@ -79,6 +79,7 @@ class OrderModel(CoreModel):
     shopify_order_number: str
     order_date: datetime
     status: Optional[str] = None
+    ship_by_date: Optional[datetime] = None
     shipping_address_line1: Optional[str] = None
     shipping_address_line2: Optional[str] = None
     shipping_city: Optional[str] = None
--- server/tests/integration/test_webhooks_order_paid_general.py
+++ server/tests/integration/test_webhooks_order_paid_general.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, datetime
+
 from parameterized import parameterized
 
 from server.services.order_service import (
@@ -50,6 +52,38 @@ class TestWebhooksOrderPaidGeneral(BaseTestCase):
         order_id = response.json["id"]
         order = self.order_service.get_order_by_id(order_id)
         self.assertIsNotNone(order)
+        self.assertEqual(order.event_id, event_id)
+        self.assertIsNotNone(order.ship_by_date)
+
+        attendee = self.attendee_service.get_attendee_by_id(attendee.id)
+        self.assertTrue(attendee.pay)
+
+    def test_order_ship_by_date_less_then_six_weeks(self):
+        # given
+        user = self.user_service.create_user(fixtures.create_user_request())
+        three_weeks = (datetime.now() + timedelta(weeks=3)).isoformat()
+        event_id = self.event_service.create_event(
+            fixtures.create_event_request(user_id=user.id, event_at=three_weeks), True
+        ).id
+        attendee_user = self.user_service.create_user(fixtures.create_user_request())
+        attendee = self.attendee_service.create_attendee(
+            fixtures.create_attendee_request(user_id=attendee_user.id, event_id=event_id, email=attendee_user.email)
+        )
+
+        # when
+        webhook_request = fixtures.webhook_shopify_paid_order(
+            customer_email=attendee_user.email,
+            line_items=[fixtures.webhook_shopify_line_item(sku=f"product-{utils.generate_unique_string()}")],
+            event_id=str(event_id),
+        )
+
+        response = self._post(WEBHOOK_SHOPIFY_ENDPOINT, webhook_request, PAID_ORDER_REQUEST_HEADERS)
+
+        # then
+        self.assert200(response)
+        order_id = response.json["id"]
+        order = self.order_service.get_order_by_id(order_id)
+        self.assertIsNone(order.ship_by_date)
         self.assertEqual(order.event_id, event_id)
 
         attendee = self.attendee_service.get_attendee_by_id(attendee.id)

b4ef2c157757903d7d3d8aeb3e5b76b84fa95e45
Max Lobur <max_lobur@outlook.com>
2024-08-22 14:50:22 +0200

    Set invite for owner on backend

--- server/tests/integration/test_attendees.py
+++ server/tests/integration/test_attendees.py
@@ -272,6 +272,25 @@ class TestAttendees(BaseTestCase):
         # then
         self.assertStatus(response, 409)
 
+    def test_create_attendee_for_owner(self):
+        # given
+        owner = self.user_service.create_user(fixtures.create_user_request())
+        event = self.event_service.create_event(fixtures.create_event_request(user_id=owner.id))
+
+        # when
+        response = self.client.open(
+            "/attendees",
+            query_string=self.hmac_query_params,
+            method="POST",
+            data=fixtures.create_attendee_request(event_id=event.id, email=owner.email).json(),
+            headers=self.request_headers,
+            content_type=self.content_type,
+        )
+
+        # then
+        self.assertStatus(response, 201)
+        self.assertTrue(response.json["invite"])
+
     def test_get_attendee_event_non_existing_user(self):
         # when
         query_params = {

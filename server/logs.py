e3b86e0cccd14c65919a555d7ee01e908287d1ca
Max Lobur <max_lobur@outlook.com>
2024-08-22 19:24:38 +0200

    Global powertools

--- server/logs.py
+++ server/logs.py
@@ -20,17 +20,16 @@ def log_shopify_id_middleware():
 def init_logging(service, debug=False):
     logger.setLevel(logging.DEBUG if debug else logging.INFO)
     logger.append_keys(service=service)
-    logger.append_keys(release=get_version() or "")
+    logger.append_keys(version=get_version() or "")
     logger.append_keys(environment=os.getenv("STAGE"))
 
     # Override root handler
     for existing_logger in logging.root.manager.loggerDict.values():
-        if isinstance(existing_logger, logging.Logger):
-            existing_logger.handlers = logger.handlers
-            existing_logger.setLevel(logger.log_level)
+        existing_logger.handlers = logger.handlers
+        existing_logger.setLevel(logger.log_level)
 
     # Tune libraries log levels
-    logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
     for name in logging.root.manager.loggerDict:
-        if name.startswith("connexion."):
-            logging.getLogger(name).setLevel(logging.INFO)
+        for mute in ["connexion.", "flask_cors.", "aws_lambda_powertools.", "sqlalchemy."]:
+            if name.startswith(mute):
+                logging.getLogger(name).setLevel(logging.INFO)

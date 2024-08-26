1ffa03f7774c9c8f91240fa65f6bdf02a0e26a93
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-08-26 16:32:38 +0300

    update

--- server/controllers/shipping.py
+++ server/controllers/shipping.py
@@ -1,13 +1,12 @@
 import logging
 
-from server.controllers.util import error_handler, log_request
+from server.controllers.util import error_handler
 from server.flask_app import FlaskApp
 
 logger = logging.getLogger(__name__)
 
 
 @error_handler
-@log_request
 def calculate_shipping_price(payload):
     shipping_service = FlaskApp.current().shipping_service
 

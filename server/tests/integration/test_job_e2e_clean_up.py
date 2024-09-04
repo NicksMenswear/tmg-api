122dabe18847a122edd1739caa54b4b581369192
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-09-04 15:15:00 +0300

    update

--- server/tests/integration/test_job_e2e_clean_up.py
+++ server/tests/integration/test_job_e2e_clean_up.py
@@ -1,14 +1,14 @@
 from server.tests.integration import BaseTestCase
 
 
-class TestEvents(BaseTestCase):
+class TestJobE2ECleanUp(BaseTestCase):
     def setUp(self):
         super().setUp()
 
-    def test_hello_world(self):
+    def test_e2e_clean_up(self):
         response = self.client.open(
-            "/jobs/hello-world",
-            query_string=self.hmac_query_params,
+            "/jobs/system/e2e-clean-up",
+            query_string={},
             method="POST",
             content_type=self.content_type,
             headers=self.request_headers,

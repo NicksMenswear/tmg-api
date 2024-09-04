77a0be50786f0e70b5ca130bbbddf45df0d0efaf
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-09-04 16:42:15 +0300

    update

--- jobs.py
+++ jobs.py
@@ -1,15 +1,18 @@
 import logging
 
+from aws_lambda_powertools.logging import correlation_paths
+
 from server.app import init_sentry
 from server.config import Config
 from server.controllers.util import http
-from server.logs import init_logging
+from server.logs import init_logging, powerlogger
 
 logger = logging.getLogger(__name__)
 
 config = Config()
 
 
+@powerlogger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
 def lambda_handler_e2e_clean_up(event, context):
     init_logging("job_e2e_clean_up", debug=True)
     init_sentry()

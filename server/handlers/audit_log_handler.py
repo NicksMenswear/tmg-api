45de0021467e334141ddd3010f64fed65adca250
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-10-08 09:04:10 +0300

    update

--- server/handlers/audit_log_handler.py
+++ server/handlers/audit_log_handler.py
@@ -6,7 +6,7 @@ from server.database.models import AuditLog
 from server.logs import init_logging, powerlogger
 from server.models.audit_model import AuditLogMessage
 
-init_logging("tmg-audit-logs-processing", debug=True)
+init_logging("tmg-audit-logs-processing", debug=False)
 
 
 logger = logging.getLogger(__name__)
@@ -18,7 +18,6 @@ def process_messages(event, context):
         message_body = record["body"]
 
         try:
-            print(f"Processing message: {message_body}")
             logger.info(f"Processing message: {message_body}")
 
             audit_log_message = AuditLogMessage.from_string(message_body)
@@ -30,7 +29,6 @@ def process_messages(event, context):
             db.session.add(audit_log)
             db.session.commit()
         except Exception as e:
-            print(f"Error processing message: {message_body}", str(e))
             logger.exception(f"Error processing message: {message_body}", e)
 
             return {"statusCode": 500, "body": json.dumps("Error processing messages")}

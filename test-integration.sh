3a45608d05c8746a975ea12b824f23a6433d3827
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-11-05 13:21:46 +0200

    update

--- test-integration.sh
+++ test-integration.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+export BROWSER=${BROWSER:-chromium}
+export VIEWPORT=${VIEWPORT:-"mobile"}
+export SHOPIFY_STORE=${SHOPIFY_STORE:-"quickstart-a91e1214"}
+export SHOPIFY_ADMIN_API_ACCESS_TOKEN=${SHOPIFY_ADMIN_API_ACCESS_TOKEN:-"shpat_1234567890abcdef"}
+
 docker-compose up --build --abort-on-container-exit test-integration

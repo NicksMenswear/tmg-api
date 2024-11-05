a25f2ccd74663d0e28cfc78e8f4d6038df9e392c
Max Lobur <max_lobur@outlook.com>
2024-11-05 16:36:17 +0100

    disable sentry in tests

--- server/handlers/__init__.py
+++ server/handlers/__init__.py
@@ -7,6 +7,9 @@ from server.version import get_version
 
 
 def init_sentry():
+    if os.getenv("TMG_APP_TESTING", "false").lower() == "true":
+        return
+
     sentry_sdk.init(
         dsn="https://8e6bac4bea5b3bf97a544417ca20e275@o4507018035724288.ingest.us.sentry.io/4507018177609728",
         environment=os.getenv("STAGE"),

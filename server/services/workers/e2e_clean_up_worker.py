4e8669105e61832fd844a782540463234d6693d7
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-11-26 17:55:49 +0200

    Fixed e2e clean up for system e2e users (#445)

--- server/services/workers/e2e_clean_up_worker.py
+++ server/services/workers/e2e_clean_up_worker.py
@@ -1,8 +1,9 @@
 import logging
 import uuid
+from logging import Logger
 from typing import List
 
-from sqlalchemy import func, select, delete
+from sqlalchemy import func, select, delete, or_
 
 from server.database.database_manager import db
 from server.database.models import (
@@ -21,8 +22,6 @@ from server.database.models import (
 )
 from server.services import ServiceError
 from server.services.integrations.shopify_service import AbstractShopifyService, ShopifyService
-
-logger = logging.getLogger(__name__)
 
 NUMBER_OF_USERS_TO_PROCESS = 30
 
@@ -43,8 +42,9 @@ CUSTOMER_EMAIL_MATCHING_PATTERN = "e2e+*@mail.dev.tmgcorp.net"
 
 
 class E2ECleanUpWorker:
-    def __init__(self, shopify_service: AbstractShopifyService):
+    def __init__(self, shopify_service: AbstractShopifyService, logger: Logger = None):
         self.shopify_service = shopify_service
+        self.logger = logger if logger else logging.getLogger(__name__)
 
     def get_customers(self, num_customers=250):
         num_customers = min(num_customers, 250)
@@ -62,13 +62,13 @@ class E2ECleanUpWorker:
     def cleanup(self, customer_gid: str, email: str) -> None:
         is_system_user = email in SYSTEM_E2E_EMAILS_TO_KEEP
 
-        logger.info(f"Processing customer: {email}")
+        self.logger.info(f"Processing customer: {email}")
 
         try:
             user = self.__get_user_by_email(email)
 
             if not user:
-                logger.error(f"User not found in db by email: {email}")
+                self.logger.error(f"User not found in db by email: {email}")
 
                 self.__delete_shopify_customer(customer_gid)
 
@@ -82,7 +82,7 @@ class E2ECleanUpWorker:
                 for discount in discounts:
                     discount_code = discount.shopify_discount_code
 
-                    logger.info(f"Deleting discount: {discount_code}")
+                    self.logger.info(f"Deleting discount: {discount_code}")
 
                     if discount.shopify_discount_code_id and discount_code:
                         self.__delete_shopify_discount(discount.shopify_discount_code_id)
@@ -93,24 +93,52 @@ class E2ECleanUpWorker:
             events = self.__get_events(user.id)
 
             for event in events:
-                num_attendees = self.__num_attendees_in_event(event.id)
+                num_active_attendees = self.__num_active_attendees_in_event(event.id)
 
-                logger.info(f"Deleting event: {event.id}")
-
-                if num_attendees > 0:
-                    logger.info(f"Event has attendees, skipping deletion: {event.id}")
+                if num_active_attendees > 0:
+                    self.logger.info(f"Event has attendees, skipping deletion: {event.id}")
                     continue
 
-                self.__delete_roles(event.id)
+                num_inactive_or_without_user_attendees = self.__num_inactive_or_without_user_attendees_in_event(
+                    event.id
+                )
 
-                orders = self.__get_orders_for_event(event.id)
+                if num_inactive_or_without_user_attendees == 0:
+                    self.logger.info(f"Deleting event: {event.id}")
 
-                for order in orders:
-                    logger.info(f"Deleting order: {order.id}")
-                    self.__delete_order_items_for_order(order.id)
-                    self.__delete_order(order.id)
+                    self.__delete_roles(event.id)
 
-                self.__delete_event(event.id)
+                    orders = self.__get_orders_for_event(event.id)
+
+                    for order in orders:
+                        self.logger.info(f"Deleting order: {order.id}")
+                        self.__delete_order_items_for_order(order.id)
+                        self.__delete_order(order.id)
+
+                    self.__delete_event(event.id)
+                else:
+                    attendees = self.__get_inactive_attendees_for_event(event.id)
+
+                    for attendee in attendees:
+                        discounts = self.__get_discounts(attendee.id)
+
+                        for discount in discounts:
+                            discount_code = discount.shopify_discount_code
+
+                            self.logger.info(f"Deleting discount: {discount_code}")
+
+                            if discount.shopify_discount_code_id and discount_code:
+                                self.__delete_shopify_discount(discount.shopify_discount_code_id)
+
+                            self.__delete_discount(discount.id)
+
+                        self.logger.info(f"Deleting attendee: {attendee.id}")
+                        self.__delete_attendee(attendee.id)
+                        try:
+                            if attendee.user_id:
+                                self.__delete_user(attendee.user_id)
+                        except Exception as e:
+                            self.logger.error(f"Failed to delete user: {attendee.user_id}")
 
                 db.session.commit()
 
@@ -126,16 +154,19 @@ class E2ECleanUpWorker:
                 if product_specs and product_specs.get("bundle", {}).get("product_id"):
                     shopify_bundle_product_id = product_specs["bundle"]["product_id"]
 
+                    self.logger.info(f"Deleting bundle product: {shopify_bundle_product_id}")
                     self.__delete_shopify_product(shopify_bundle_product_id)
 
                     for item in product_specs.get("items", []):
                         if not item.get("variant_sku", "").startswith("bundle-"):
                             continue
 
+                        self.logger.info(f"Deleting product: {item.get('product_id')}")
                         self.__delete_shopify_product(item.get("product_id"))
 
                 attendees_with_look = self.__get_attendees_with_look(look.id)
                 if not attendees_with_look:
+                    self.logger.info(f"Deleting look: {look.id}/{look.name}")
                     self.__delete_look(look.id)
 
                 db.session.commit()
@@ -144,7 +175,7 @@ class E2ECleanUpWorker:
                 db.session.commit()
                 return
 
-            logger.info(f"Deleting customer: {email}")
+            self.logger.info(f"Deleting customer: {email}")
 
             self.__delete_shopify_customer(customer_gid)
             self.__delete_user_activity_logs(user.id)
@@ -153,7 +184,7 @@ class E2ECleanUpWorker:
             db.session.commit()
         except Exception as e:
             db.session.rollback()
-            logger.exception(f"Failed to process customer: {email}")
+            self.logger.exception(f"Failed to process customer: {email}")
 
     def bulk_cleanup(self) -> None:
         customers = self.get_customers(NUMBER_OF_USERS_TO_PROCESS)
@@ -165,23 +196,31 @@ class E2ECleanUpWorker:
         try:
             self.shopify_service.delete_customer(customer_gid)
         except ServiceError:
-            logger.error(f"Failed to delete customer from shopify: {customer_gid}")
+            self.logger.error(f"Failed to delete customer from shopify: {customer_gid}")
 
     def __delete_shopify_product(self, product_id: int) -> None:
         try:
             self.shopify_service.delete_product(ShopifyService.product_gid(product_id))
         except ServiceError:
-            logger.exception(f"Failed to delete product from shopify: {product_id}")
+            self.logger.exception(f"Failed to delete product from shopify: {product_id}")
 
     def __delete_shopify_discount(self, discount_id: int) -> None:
         try:
             self.shopify_service.delete_discount(f"gid://shopify/DiscountCodeNode/{discount_id}")
         except ServiceError:
-            logger.error(f"Failed to delete discount from shopify: {discount_id}")
+            self.logger.error(f"Failed to delete discount from shopify: {discount_id}")
 
     @staticmethod
-    def __num_attendees_in_event(event_id: uuid.UUID) -> int:
-        return db.session.execute(select(func.count(Attendee.id)).where(Attendee.event_id == event_id)).scalar()  # type: ignore
+    def __num_active_attendees_in_event(event_id: uuid.UUID) -> int:
+        return db.session.execute(select(func.count(Attendee.id)).where(Attendee.event_id == event_id, Attendee.is_active, Attendee.user_id.isnot(None))).scalar()  # type: ignore
+
+    @staticmethod
+    def __num_inactive_or_without_user_attendees_in_event(event_id: uuid.UUID) -> int:
+        return db.session.execute(
+            select(func.count(Attendee.id)).where(
+                Attendee.event_id == event_id, or_(Attendee.is_active.is_(False), Attendee.user_id.is_(None))
+            )
+        ).scalar()  # type: ignore
 
     @staticmethod
     def __get_user_by_email(email: str) -> User:
@@ -194,6 +233,10 @@ class E2ECleanUpWorker:
     @staticmethod
     def __get_attendees(user_id: uuid.UUID) -> List[Attendee]:
         return db.session.execute(select(Attendee).where(Attendee.user_id == user_id)).scalars().all()  # type: ignore
+
+    @staticmethod
+    def __get_inactive_attendees_for_event(event_id: uuid.UUID) -> List[Attendee]:
+        return db.session.execute(select(Attendee).where(Attendee.event_id == event_id, or_(Attendee.is_active.is_(False), Attendee.user_id.is_(None)))).scalars().all()  # type: ignore
 
     @staticmethod
     def __get_attendees_with_look(look_id: uuid.UUID) -> List[Attendee]:

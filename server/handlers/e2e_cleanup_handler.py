f282586befe01adeb2ae5b35dd7ae6afd286a4f1
Zinovii Dmytriv <zdmytriv@gmail.com>
2024-11-26 17:12:38 +0200

    update

--- server/handlers/e2e_cleanup_handler.py
+++ server/handlers/e2e_cleanup_handler.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
@@ -38,15 +39,24 @@ class FakeLambdaContext(LambdaContext):
 
 @logger.inject_lambda_context
 def lambda_handler(event: dict, context: LambdaContext):
-    e2e_clean_up_worker = E2ECleanUpWorker(ShopifyService(ONLINE_STORE_SALES_CHANNEL_ID))
+    e2e_clean_up_worker = E2ECleanUpWorker(ShopifyService(ONLINE_STORE_SALES_CHANNEL_ID), logger)
     customers = e2e_clean_up_worker.get_customers(20)
 
-    for customer in customers:
-        if customer.get("email") in SYSTEM_E2E_EMAILS_TO_KEEP:
-            logger.info(f"Skipping system customer: {customer.get('email')}")
-            continue
+    non_system_customers = [
+        customer for customer in customers if customer.get("email") not in SYSTEM_E2E_EMAILS_TO_KEEP
+    ]
 
+    for customer in non_system_customers:
         logger.info(f"Processing customer: {customer.get('email')}")
+
+        e2e_clean_up_worker.cleanup(customer.get("id"), customer.get("email"))
+
+    system_customers = [customer for customer in customers if customer.get("email") in SYSTEM_E2E_EMAILS_TO_KEEP]
+    random.shuffle(system_customers)
+
+    # processing only 3 at a time of system e2e customers
+    for customer in system_customers[: min(3, len(system_customers))]:
+        logger.info(f"Processing system customer: {customer.get('email')}")
 
         e2e_clean_up_worker.cleanup(customer.get("id"), customer.get("email"))
 
@@ -57,5 +67,5 @@ def __in_test_context(context) -> bool:
     return isinstance(context, FakeLambdaContext)
 
 
-# if __name__ == "__main__":
-#     lambda_handler({}, FakeLambdaContext())
+if __name__ == "__main__":
+    lambda_handler({}, FakeLambdaContext())
